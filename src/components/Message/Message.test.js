8a592dfe3d5fab5c0bff70472d8c8d29f9af5122
edfranco <edfranco415@gmail.com>
2023-08-13 23:35:42 -0700

    seperated tests and added snapshot

--- src/components/Message/Message.test.js
+++ src/components/Message/Message.test.js
@@ -1,42 +1,54 @@
 import { render, screen, cleanup } from "@testing-library/react";
-import '@testing-library/jest-dom';
+import renderer from 'react-test-renderer';
+import "@testing-library/jest-dom";
 
 import Message from "./Message";
 import getMessageTime from "../../functions/getMessageTime";
 
-
-let testMessage = <Message 
-image=''
-author='Eduardo Franco'
-timeStamp= {getMessageTime()}
-body='I am a message!'
-/>;
+let testMessage = (
+  <Message
+    image=""
+    author="Eduardo Franco"
+    timeStamp={getMessageTime()}
+    body="I am a message!"
+  />
+);
 
 afterEach(() => {
-    cleanup();
+  cleanup();
 });
 
 describe("Message component", () => {
-    render(testMessage);
+  render(testMessage);
 
-    const message = screen.getByTestId("message");
-    const author = screen.getByTestId("message-author");
-    const messageTime = screen.getByTestId("message-time");
-    const messageBody = screen.getByTestId("message-body");
-    const messageImage = screen.getByTestId("message-image");
-    
- 
-    test("Message Rendering", () => {
-        expect(message).toBeInTheDocument();
-        expect(author).toBeInTheDocument();
-        expect(messageTime).toBeInTheDocument();
-        expect(messageBody).toBeInTheDocument();
-        expect(messageImage).toBeInTheDocument();
-    })
- 
-    test("Message info", () => {
-        expect(author).toHaveTextContent("Eduardo Franco");
-        expect(messageBody).toHaveTextContent("I am a message!");
-        expect(messageTime.textContent).toMatch(/(AM|PM)/i);
-    })
-})
+  const message = screen.getByTestId("message");
+  const author = screen.getByTestId("message-author");
+  const messageTime = screen.getByTestId("message-time");
+  const messageBody = screen.getByTestId("message-body");
+  const messageImage = screen.getByTestId("message-image");
+
+  test("Message Rendering", () => {
+    expect(message).toBeInTheDocument();
+    expect(author).toBeInTheDocument();
+    expect(messageTime).toBeInTheDocument();
+    expect(messageBody).toBeInTheDocument();
+    expect(messageImage).toBeInTheDocument();
+  });
+
+  test("Message author", () => {
+    expect(author).toHaveTextContent("Eduardo Franco");
+  });
+  test("Message body", () => {
+    expect(messageBody).toHaveTextContent("I am a message!");
+  });
+  test("Message timestamp", () => {
+    expect(messageTime.textContent).toMatch(/(AM|PM)/i);
+  });
+});
+
+it("Message renders correctly", () => {
+  const tree = renderer
+    .create(testMessage)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
